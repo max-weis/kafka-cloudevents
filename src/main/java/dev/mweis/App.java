669efc3045d9492bfdd5442de32f6884589e04aa
max.weis <max.weis@openknowledge.de>
2021-04-16 13:10:29 +0200

    init commit

---
+++ src/main/java/dev/mweis/App.java
@@ -0,0 +1,49 @@
+package dev.mweis;
+
+import io.smallrye.mutiny.Multi;
+import io.smallrye.reactive.messaging.ce.IncomingCloudEventMetadata;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+import static org.jboss.resteasy.resteasy_jaxrs.i18n.LogMessages.LOGGER;
+
+public class App {
+
+    @Outgoing("cloudevents-out")
+    public Multi<String> toCloudEvents() {
+        return Multi.createFrom().ticks().every(Duration.ofSeconds(1))
+            .map(x -> "Hello World");
+    }
+
+    @Incoming("cloudevents-in")
+    public CompletionStage<Void> process(Message<String> priceInUsd) {
+        IncomingCloudEventMetadata<Integer> cloudEventMetadata = priceInUsd.getMetadata(IncomingCloudEventMetadata.class)
+            .orElseThrow(() -> new IllegalArgumentException("Expected a Cloud Event"));
+
+        System.out.println(String.format("Received Cloud Events (spec-version: %s): source:  '%s', type: '%s', subject: '%s' , data: '%s'",
+            cloudEventMetadata.getSpecVersion(),
+            cloudEventMetadata.getSource(),
+            cloudEventMetadata.getType(),
+            cloudEventMetadata.getSubject().orElse("no subject"),
+            cloudEventMetadata.getData()));
+
+        return priceInUsd.ack();
+    }
+
+    @Incoming("cloudevents-in")
+    public CompletionStage<Void> process2(Message<String> priceInUsd) {
+        System.out.println(priceInUsd.getPayload());
+
+        return priceInUsd.ack();
+    }
+}
