97e1e321124e239b0c177a8abba0486525bfb459
max.weis <max.weis@openknowledge.de>
2021-04-16 13:20:38 +0200

    optimize imports

--- src/main/java/dev/mweis/App.java
+++ src/main/java/dev/mweis/App.java
@@ -6,16 +6,8 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Message;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
 import java.time.Duration;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
-
-import static org.jboss.resteasy.resteasy_jaxrs.i18n.LogMessages.LOGGER;
 
 public class App {
 
